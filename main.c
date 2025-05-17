dd376987ac616d508c6c74b652c36cc4828441ca
datagore <104622881+datagore@users.noreply.github.com>
2025-05-17 22:03:24 +0300

    More random doodling

--- main.c
+++ main.c
@@ -1,182 +1,156 @@
-// TODO: Write operations to an array before outputting or counting them
-// TODO: Optimize by merging or eliminating redundant operations
-// FIXME: Input currently consists of all numbers between 0 and N
-
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 // ANSI escape codes.
-#define ANSI_GREEN	"\x1b[1;32m" // Set the text color to green.
-#define ANSI_RED	"\x1b[1;31m" // Set the text color to red.
-#define ANSI_RESET	"\x1b[0m"	 // Reset to default color.
-#define ANSI_CLEAR	"\x1b[2J"	 // Clear the screen.
+#define ANSI_GREEN  "\x1b[1;32m" // Set the text color to green.
+#define ANSI_RED    "\x1b[1;31m" // Set the text color to red.
+#define ANSI_RESET  "\x1b[0m"    // Reset to default color.
+#define ANSI_CLEAR  "\x1b[2J"    // Clear the screen.
 
 // Color-coded OK and KO strings.
 #define GREEN_OK (ANSI_GREEN "[OK]" ANSI_RESET)
-#define RED_KO	 (ANSI_RED	 "[KO]" ANSI_RESET)
+#define RED_KO   (ANSI_RED   "[KO]" ANSI_RESET)
 
 void random_permutation(int *array, int length)
 {
-	for (int i = 0; i < length; i++)
-		array[i] = i;
-	for (int i = 0; i < length - 1; i++) {
-		int j = rand() % (length - i) + i;
-		int temporary = array[j];
-		array[j] = array[i];
-		array[i] = temporary;
+    for (int i = 0; i < length; i++)
+        array[i] = i;
+    for (int i = 0; i < length - 1; i++) {
+        int j = rand() % (length - i) + i;
+        int temporary = array[j];
+        array[j] = array[i];
+        array[i] = temporary;
+    }
+}
+
+typedef struct s_stack {
+	int *data;
+	int cap;
+	int len;
+	int top;
+} t_stack;
+
+int stack_get(const t_stack *s, int index)
+{
+	return s->data[(s->top + s->len * (index < 0) + index) % s->cap];
+}
+
+void stack_set(t_stack *s, int index, int value)
+{
+	s->data[(s->top + s->len * (index < 0) + index) % s->cap] = value;
+}
+
+void stack_push(t_stack *dst, t_stack *src)
+{
+	if (src->len > 0) {
+		int value = src->data[src->top];
+		src->top = (src->top + 1) % src->cap;
+		dst->top = (dst->top - 1 + dst->cap) % dst->cap;
+		dst->data[dst->top] = value;
+		dst->len++;
+		src->len--;
 	}
 }
 
-typedef struct s_state {
-	int *stack[2];
-	int top[2];
-	int len[2];
-	int operation_count;
-	int wrap;
-} t_state;
+void stack_swap(t_stack *s)
+{
+	if (s->len > 1) {
+		int temp = stack_get(s, 0);
+		stack_set(s, 0, stack_get(s, 1));
+		stack_set(s, 1, temp);
+	}
+}
+
+void stack_rotate(t_stack *s, int step)
+{
+	if (step == -1)
+		stack_set(s, s->len, stack_get(s, 0));
+	s->top = (s->top - step + s->cap) % s->cap;
+	if (step == +1)
+		stack_set(s, 0, stack_get(s, s->len));
+}
+
+void stack_print(const t_stack *s)
+{
+	printf("[");
+	for (int i = 0; i < s->len; i++)
+		printf("%s%d", i ? ", " : "", stack_get(s, i));
+	printf("]\n");
+}
 
 enum {sa, sb, ss, pa, pb, ra, rb, rr, rra, rrb, rrr};
 
-void display_state(const t_state *s)
+void print_move(int move_id)
 {
-	for (int i = 0; i < 2; i++) {
-		printf("%c:", "AB"[i]);
-		for (int j = 0; j < s->len[i]; j++)
-			printf(" %d", s->stack[i][(s->top[i] + j) % s->wrap]);
-		printf("\n");
-	}
-	printf("\n");
+	static const char* const move_names[] = {
+		"sa\n", "sb\n", "ss\n", "pa\n", "pb\n", "ra\n", "rb\n", "rr\n", "rra\n",
+		"rrb\n", "rrr\n"
+	};
+	fflush(stdout); // FIXME: Remove
+	write(1, move_names[move_id], 3 + (move_id >= rra));
 }
 
-void swap(int *a, int *b)
+static int move_count;
+
+void perform_move(t_stack *a, t_stack *b, int move)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	print_move(move);
+	if (move == pa)
+		stack_push(a, b);
+	if (move == pb)
+		stack_push(b, a);
+	if (move == sa || move == ss)
+		stack_swap(a);
+	if (move == sb || move == ss)
+		stack_swap(b);
+	if (move == ra || move == rr)
+		stack_rotate(a, -1);
+	if (move == rb || move == rr)
+		stack_rotate(b, -1);
+	if (move == rra || move == rrr)
+		stack_rotate(a, +1);
+	if (move == rrb || move == rrr)
+		stack_rotate(b, +1);
+	move_count++;
 }
 
-int stack_is_sorted(t_state *s, int dst)
+void sort(t_stack *a, t_stack *b)
 {
-	for (int i = 1; i < s->len[dst]; i++)
-		if (s->stack[dst][(s->top[dst] + i - 1) % s->wrap] >= s->stack[dst][(s->top[dst] + i) % s->wrap])
-			return 0;
-	return 1;
-}
-
-void stack_swap(t_state *s, int dst)
-{
-	if (s->len[dst] > 1)
-	{
-		int i = (s->top[dst] + 0) % s->wrap;
-		int j = (s->top[dst] + 1) % s->wrap;
-		int temp = s->stack[dst][i];
-		s->stack[dst][i] = s->stack[dst][j];
-		s->stack[dst][j] = temp;
-	}
-}
-
-void stack_push(t_state *s, int dst, int src)
-{
-	if (s->len[src] > 0)
-	{
-		s->top[dst] = (s->top[dst] + s->wrap - 1) % s->wrap;
-		s->stack[dst][s->top[dst]] = s->stack[src][s->top[src]];
-		s->top[src] = (s->top[src] + 1) % s->wrap;
-		s->len[dst]++;
-		s->len[src]--;
-	}
-}
-
-void stack_rotate(t_state *s, int dst, int step)
-{
-	if (step == 1)
-	{
-		s->top[dst] = (s->top[dst] + s->wrap - step) % s->wrap;
-		s->stack[dst][s->top[dst]] = s->stack[dst][(s->top[dst] + s->len[dst]) % s->wrap];
-	}
-	else if (step == -1)
-	{
-		s->top[dst] = (s->top[dst] + s->wrap - step) % s->wrap;
-		s->stack[dst][(s->top[dst] + s->len[dst] + step) % s->wrap] = s->stack[dst][(s->top[dst] + s->wrap + step) % s->wrap];
-	}
-}
-
-void run_operation(t_state *s, int op)
-{
-	if (op == sa || op == ss)
-		stack_swap(s, 0);
-	if (op == sb || op == ss)
-		stack_swap(s, 1);
-	if (op == pa)
-		stack_push(s, 0, 1);
-	if (op == pb)
-		stack_push(s, 1, 0);
-	if (op == ra || op == rr)
-		stack_rotate(s, 0, -1);
-	if (op == rb || op == rr)
-		stack_rotate(s, 1, -1);
-	if (op == rra || op == rrr)
-		stack_rotate(s, 0, +1);
-	if (op == rrb || op == rrr)
-		stack_rotate(s, 1, +1);
-	s->operation_count++;
-}
-
-int index_of_value(t_state *s, int stack, int value)
-{
-	for (int i = 0; i < s->len[stack]; i++)
-		if (s->stack[stack][(s->top[stack] + i) % s->wrap] == value)
-			return i;
-	return -1;
-}
-
-// Dumb sort, rotates until finding the next value in the order.
-void sort(t_state *s)
-{
-	const int input_length = s->len[0];
-	for (int i = 0; i < input_length; i++) {
-		int a = index_of_value(s, 0, i);
-		int b = index_of_value(s, 1, i);
-		if (a == 0) {
-			run_operation(s, ra);
-			continue;
-		} else if (a > 0) {
-			run_operation(s, pb);
-		} else if (b == 0) {
-			run_operation(s, pa);
-		} else if (b > 0) {
-			if (b * 2 > s->len[1]) {
-				run_operation(s, rrb);
-			} else {
-				run_operation(s, rb);
-			}
-		}
-		i--;
+	const int length = a->len;
+	while (a->len > 0)
+		perform_move(a, b, pb);
+	for (int i = length - 1; i >= 0; i--) {
+		while (stack_get(b, 0) != i)
+			perform_move(a, b, rb);
+		perform_move(a, b, pa);
 	}
 }
 
 int main(void)
 {
-	const int length = 5;
-	int stacks[2][length * 2];
-	random_permutation(stacks[0], length);
-	t_state state = {
-		.stack = {stacks[0], stacks[1]},
-		.len[0] = length,
-		.wrap = length * 2,
-	};
+	const int length = 100;
+	int array[length * 4];
+	random_permutation(array, length);
+
+	t_stack stacks[2];
+	for (int i = 0; i < 2; i++) {
+		stacks[i].data = array + length * i * 2;
+		stacks[i].len = length * (1 - i);
+		stacks[i].cap = length * 2;
+		stacks[i].top = 0;
+	}
+
 	printf("Input:\n");
-	display_state(&state);
-	sort(&state);
-	printf("Output:\n");
-	display_state(&state);
-	int is_sorted = stack_is_sorted(&state, 0);
-	printf("Total operations: %d\n", state.operation_count);
-	printf("Order: %s\n", is_sorted ? GREEN_OK : RED_KO);
-	printf("Moves: %d", state.operation_count);
-	printf(" (%d%% of target)", state.operation_count * 100 / 5500);
-	printf(" %s\n", state.operation_count < 5500 ? GREEN_OK : RED_KO);
+	stack_print(&stacks[0]);
+	stack_print(&stacks[1]);
+	sort(&stacks[0], &stacks[1]);
+	printf("\nOutput:\n");
+	stack_print(&stacks[0]);
+	stack_print(&stacks[1]);
+
+	printf("\nMoves: %d\n", move_count);
 }
