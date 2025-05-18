a7f68abb8683588d7ff191bf3e2962c9c5a52f76
datagore <104622881+datagore@users.noreply.github.com>
2025-05-18 10:08:25 +0300

    Still busted lol

--- main.c
+++ main.c
@@ -90,7 +90,11 @@ void print_move(int move_id)
 		"sa\n", "sb\n", "ss\n", "pa\n", "pb\n", "ra\n", "rb\n", "rr\n", "rra\n",
 		"rrb\n", "rrr\n"
 	};
+#if 0
 	write(1, move_names[move_id], 3 + (move_id >= rra));
+#else
+	(void) move_names, (void) move_id;
+#endif
 }
 
 static int move_count;
@@ -124,30 +128,71 @@ int min(int a, int b)
 	return b;
 }
 
-int find_place(t_stack *a, t_stack *b, int b_index)
+int find_index_of_min_value(t_stack *a)
 {
-	int a_index = 0;
-	const int b_value = stack_get(b, b_index);
-	while (a->len > 0 && stack_get(a, a_index) < b_value)
-		a_index++;
-	return a_index;
+	int min_index = 0;
+	int min_value = INT_MAX;
+	for (int index = 0; index < a->len; index++) {
+		int value = stack_get(a, index);
+		if (value < min_value) {
+			min_value = value;
+			min_index = index;
+		}
+	}
+	return min_index;
 }
 
-int calculate_cost(t_stack *a, t_stack *b, int rb)
+int find_place(t_stack *a, t_stack *b, int b_index)
 {
-	int ra = find_place(a, b, rb);
-	int rra = a->len - ra;
-	int rrb = b->len - rb;
-	return min(ra, rra) + min(ra, rrb);
+	// TODO: Use binary search somehow.
+	int b_value = stack_get(b, b_index);
+	int a_index = find_index_of_min_value(a);
+	for (int i = 0; i < a->len && stack_get(a, a_index) < b_value; i++, a_index++);
+	return a_index % a->len;
+}
+
+int move_number(t_stack *a, t_stack *b, int b_index, int move)
+{
+	/*
+	int rb_score = b_index;
+	int ra_score = find_place(a, b, b_index);
+	int rra_score = a->len - ra_score;
+	int rrb_score = b->len - rb_score;
+	int score = min(ra_score, rra_score) + min(ra_score, rrb_score);
+	if (move) {
+		for (int i = 0; ra_score <= rra_score && i < ra_score; i++)
+			perform_move(a, b, ra);
+		for (int i = 0; ra_score > rra_score && i < rra_score; i++)
+			perform_move(a, b, rra);
+		for (int i = 0; rb_score <= rrb_score && i < rb_score; i++)
+			perform_move(a, b, rb);
+		for (int i = 0; rb_score > rrb_score && i < rrb_score; i++)
+			perform_move(a, b, rrb);
+	}
+	*/
+	int score = 0;
+	int a_index = find_place(a, b, b_index);
+	printf("Move b(%d) before a(%d):\n", stack_get(b, b_index), stack_get(a, a_index));
+	printf("    "); stack_print(a);
+	printf("    "); stack_print(b);
+	for (int i = 0; i < a_index; i++, score++)
+		if (move)
+			perform_move(a, b, ra);
+	for (int i = 0; i < b_index; i++, score++)
+		if (move)
+			perform_move(a, b, rb);
+	if (move)
+		perform_move(a, b, pa);
+	return score;
 }
 
 int get_index_of_best_move(t_stack *a, t_stack *b)
 {
+	int best_index;
 	int best_score = INT_MAX;
-	int best_index = 0;
 	for (int index = 0; index < b->len; index++) {
-		int score = calculate_cost(a, b, index);
-		if (score < best_score) {
+		int score = move_number(a, b, index, 0);
+		if (score <= best_score) {
 			best_score = score;
 			best_index = index;
 		}
@@ -176,17 +221,27 @@ void rotate_to_index(t_stack *a, t_stack *b, int index)
 void sort(t_stack *a, t_stack *b)
 {
 	const int length = a->len;
-	while (a->len > 0)
+	while (a->len > 2)
 		perform_move(a, b, pb);
+	if (stack_get(a, 0) > stack_get(a, 1))
+		perform_move(a, b, sa);
+#if 1
+	while (b->len > 0) {
+		// int b_index = get_index_of_best_move(a, b);
+		int b_index = 0;
+		move_number(a, b, b_index, 1);
+	}
+#else
 	for (int i = length - 1; i >= 0; i--) {
 		rotate_to_index(a, b, get_index_of_value(b, i));
 		perform_move(a, b, pa);
 	}
+#endif
 }
 
 int main(void)
 {
-	const int length = 100;
+	const int length = 11;
 	int array[length * 4];
 	random_permutation(array, length);
 
@@ -198,11 +253,18 @@ int main(void)
 		stacks[i].top = 0;
 	}
 
+#if 0
+	for (int i = 0; i < length / 2; i++)
+		perform_move(&stacks[0], &stacks[1], pb);
+#endif
+
+	printf(ANSI_CLEAR);
 	printf("Input:\n");
 	fflush(stdout);
 	stack_print(&stacks[0]);
 	stack_print(&stacks[1]);
 	sort(&stacks[0], &stacks[1]);
+	// printf("\nFound: %d\n", find_place(&stacks[0], &stacks[1], 0));
 	printf("\nOutput:\n");
 	stack_print(&stacks[0]);
 	stack_print(&stacks[1]);
